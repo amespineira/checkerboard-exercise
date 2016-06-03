0c078a4be8fe15d201ac1267dec1d37b6a24ce4f
amespineira <amespineira@me.com>
2016-06-03 10:50:55 -0600

    solved gradient

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 // Your JS goes here
 var body=document.getElementsByTagName('body');
-
-createBoard([9,7],'red','black')
+var rgb1=[179, 179, 255];
+var rgb2=[230, 179, 204];
+createBoard([9,7])
 
 function createBoard(size, color1, color2){ //size is formatted: rowlength, columns
     for(var i=1; i<=size[1]; i++){
-      (i%2===0)? createRow(color1,color2,size[0]): createRow(color2,color1,size[0]);
+      subtract(i-1);
+      (i%2===0)? createRow("rgb("+rgb1.join(",")+")","rgb("+rgb2.join(",")+")",size[0]): createRow("rgb("+rgb2.join(",")+")","rgb("+rgb1.join(",")+")",size[0]);
     }
 }
+function subtract(i){
+  rgb1[0]-=i*10
+  rgb1[1]-=i*10
+  rgb1[2]-=i*10
+  rgb2[0]-=i*10
+  rgb2[1]-=i*10
+  rgb2[2]-=i*10
+}
 function createRow(color1, color2, length){
+  console.log(color1);
+  console.log(color2);
   for(var i=1; i<=length; i++){
     (i%2===0)? createBlock(color2) : createBlock(color1);
   }
